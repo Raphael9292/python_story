b43b7622e2a57e5784c152087164b9dfa77147d0
raphael9292 <kyeongseong.kim@kurlycorp.com>
2021-10-23 23:57:03 +0900

    feat(lottobuy.py): exception
    * NoSuchElementException 분리
    * try, except, finally 분기
    
    Signed-off-by: raphael9292 <kyeongseong.kim@kurlycorp.com>

--- lottoBuy.py
+++ lottoBuy.py
@@ -8,6 +8,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
 
 ID = config.IDENTITY
 PW = config.PASSWORD
@@ -62,62 +63,80 @@ for i in range(size):
 
 driver.switch_to.window(main_handle)
 
-# 구매창 이동
-driver.get('https://el.dhlottery.co.kr/game/TotalGame.jsp?LottoId=LO40')
-
 # 자동번호발급 이동: UnexpectedAlertPresentException
 try:
+    # 구매창 이동
+    driver.get('https://el.dhlottery.co.kr/game/TotalGame.jsp?LottoId=LO40')
     driver.switch_to.frame('ifrm_tab')
     driver.find_element_by_xpath('//*[@id="num2"]').click()
 
-# 실패시 에러원인 슬랙 & 세션 종료
+    # 적용 수량 선택
+    select = Select(driver.find_element_by_xpath('//*[@id="amoundApply"]'))
+    select.select_by_value(AUTO_LOTTO_COUNT)
+
+    # 확인 버튼 클릭
+    driver.find_element_by_xpath('//*[@id="btnSelectNum"]').click()
+
+    # 나의로또번호 이동: driver.switch_to.frame('ifrm_tab')
+    driver.find_element_by_xpath('//*[@id="num4"]').click()
+
+    # myList 선택
+    # selenium.common.exceptions.NoSuchElementException: Message: no such element: Unable to locate element
+    wait(driver, 3).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="myList"]/li[1]/input'))).click()
+    # driver.find_element_by_xpath('//*[@id="myList"]/li[1]/input').click()
+    driver.find_element_by_xpath('//*[@id="myList"]/li[2]/input').click()
+    driver.find_element_by_xpath('//*[@id="myList"]/li[3]/input').click()
+
+    # 확인 버튼 클릭
+    driver.find_element_by_xpath('//*[@id="divWay2Buy3"]/div[2]/input[1]').click()
+
+    # 구매하기 클릭
+    driver.find_element_by_xpath('//*[@id="btnBuy"]').click()
+
+    # 구매하시겠습니까? 확인 팝업 클릭, 왜 알람창을 인식하지 못하는가...
+    # selenium.common.exceptions.NoAlertPresentException: Message: no such alert
+    # alert = driver.switch_to.alert()
+    # alert.accept()
+
+    # buy[1]
+    driver.find_element_by_xpath('//*[@id="popupLayerConfirm"]/div/div[2]/input[1]').click()
+
+    # cancel[2]
+    # driver.find_element_by_xpath('//*[@id="popupLayerConfirm"]/div/div[2]/input[2]').click()
+
+    # 성공 슬랙
+    success_data = {
+        "username": "successBuyLotto",
+        "text": '<@U025Y7G6W65> ' + 'Check your lotto result \n https://dhlottery.co.kr/userSsl.do?method=myPage'
+    }
+    req = requests.post(SLACK_WEBHOOK_URL, headers=HEADERS, data=json.dumps(success_data))
+    print("Success")
+
+# repr: https://docs.python.org/3/library/functions.html#repr
+# str: https://docs.python.org/3/library/functions.html#func-str
+# error 클래스를 확인하기 위해 repr 사용
+
+# 회차정보 없을때 catch
+except NoSuchElementException as e:
+    fail_data = {
+        "username": "failBuyLotto",
+        "text": '<@U025Y7G6W65> ' + repr(e) + "\n회차정보가 존재하지 않습니다."
+    }
+    req = requests.post(SLACK_WEBHOOK_URL, headers=HEADERS, data=json.dumps(fail_data))
+    print(repr(e))
+    print("Fail")
+
+# 보편적 에러
 except Exception as e:
     fail_data = {
         "username": "failBuyLotto",
-        "text": '<@U025Y7G6W65> ' + str(e)
+        "text": '<@U025Y7G6W65> ' + repr(e)
     }
     req = requests.post(SLACK_WEBHOOK_URL, headers=HEADERS, data=json.dumps(fail_data))
-    driver.close()
-
-# 적용 수량 선택
-select = Select(driver.find_element_by_xpath('//*[@id="amoundApply"]'))
-select.select_by_value(AUTO_LOTTO_COUNT)
-
-# 확인 버튼 클릭
-driver.find_element_by_xpath('//*[@id="btnSelectNum"]').click()
-
-# 나의로또번호 이동: driver.switch_to.frame('ifrm_tab')
-driver.find_element_by_xpath('//*[@id="num4"]').click()
-
-# myList 선택
-# selenium.common.exceptions.NoSuchElementException: Message: no such element: Unable to locate element
-wait(driver, 3).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="myList"]/li[1]/input'))).click()
-# driver.find_element_by_xpath('//*[@id="myList"]/li[1]/input').click()
-driver.find_element_by_xpath('//*[@id="myList"]/li[2]/input').click()
-driver.find_element_by_xpath('//*[@id="myList"]/li[3]/input').click()
-
-# 확인 버튼 클릭
-driver.find_element_by_xpath('//*[@id="divWay2Buy3"]/div[2]/input[1]').click()
-
-# 구매하기 클릭
-driver.find_element_by_xpath('//*[@id="btnBuy"]').click()
-
-# 구매하시겠습니까? 확인 팝업 클릭, 왜 알람창을 인식하지 못하는가...
-# selenium.common.exceptions.NoAlertPresentException: Message: no such alert
-# alert = driver.switch_to.alert()
-# alert.accept()
-
-driver.find_element_by_xpath('//*[@id="popupLayerConfirm"]/div/div[2]/input[1]').click()
-# driver.find_element_by_xpath('//*[@id="popupLayerConfirm"]/div/div[2]/input[2]').click()
-
-# 성공 슬랙
-success_data = {
-    "username": "successBuyLotto",
-    "text": '<@U025Y7G6W65> ' + 'Check your lotto result \n https://dhlottery.co.kr/userSsl.do?method=myPage'
-}
-req = requests.post(SLACK_WEBHOOK_URL, headers=HEADERS, data=json.dumps(success_data))
+    print("Fail")
 
 # chrome 종료
-driver.close()
+finally:
+    driver.close()
 
 # TODO: 예치금 부족이라면?
